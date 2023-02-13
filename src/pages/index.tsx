0a99522d7a14726013f4ab950f45114495d4d453
Adam Goodrich <agoodrich57@gmail.com>
2023-02-13 18:37:10 -0500

    component setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '@/styles/Home.module.scss';
 
 const Home = () => {
   return (
@@ -10,7 +9,7 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main></main>
     </>
   );
 };
