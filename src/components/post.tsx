a68f9a8a94a6171d1b022ed75fec37065dc83212
Hayden Tofts <haydenized@gmail.com>
2023-02-14 14:34:24 -0500

    Add basic post html

---
+++ src/components/post.tsx
@@ -0,0 +1,26 @@
+function Post() {
+  return (
+    <article className="post">
+      <small>
+        r/Test <span className="post__by-line">Posted by u/Test 10 minutes ago</span>
+      </small>
+      <h3>Default Post</h3>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book. It has survived not
+        only five centuries, but also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularized in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum.
+      </p>
+      <div className="post__sidebar">
+        <button>Upvote</button>
+        <p>0</p>
+        <button>Downvote</button>
+      </div>
+    </article>
+  );
+}
+
+export default Post;
