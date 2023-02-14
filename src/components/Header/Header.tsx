1290758f7f92c290c4255d23e591e94517da2756
bsherbrook <bsherbrook@gmail.com>
2023-02-13 20:01:29 -0500

    RC-11/build basic header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,17 @@
+import styles from 'Header.module.scss';
 
 const Header = () => {
 
     return (
         <header>
-        <div className="header__logo">
-            <button className="logo">:)__reddit</button>
+        <div className={styles.header__logo}>
+            <button className={styles.logo}>:)__reddit</button>
         </div>
-        <input type="text" placeholder="Search Reddit" className="search-bar"/>
-        <div className="header__right">
+        <input type="text" placeholder="Search Reddit" className={styles.search_bar}/>
+        <div className={styles.header__right}>
             <button>Get App</button>
             <button>Log In</button>
-            <button className="profile">:)</button>
+            <button className={styles.profile}>:)</button>
         </div>
         </header>
     );
