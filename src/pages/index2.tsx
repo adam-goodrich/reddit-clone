612c3aeac58fd568a478321486a4ca657f2d137f
Adam Goodrich <agoodrich57@gmail.com>
2023-02-15 16:56:58 -0500

    RC-12 material modal added

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -5,6 +5,9 @@ import { decrement, increment, selectValue } from 'slices/counterSlice';
 import { selectIsEven, setIsEven } from 'slices/isEvenSlice';
 import { connectToDatabase } from 'util/mongodb';
 import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 interface User {
   username: string;
@@ -17,11 +20,27 @@ interface Props {
   users: User[];
 }
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const Home: React.FC<Props> = ({ users }) => {
   const [user, setUser] = useState<User>(users[0]);
   const count = useSelector(selectValue);
   const isEven = useSelector(selectIsEven);
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -51,13 +70,29 @@ const Home: React.FC<Props> = ({ users }) => {
       </Head>
       <main className={styles.main}>
         <h1>Hello World! Jira testing 2</h1>
-        <h2>Username: {user.username}</h2>
-        <h2>Email: {user.email}</h2>
-        <h2>Password: {user.password}</h2>
-        <h2>Id: {user._id}</h2>
-        <button className={styles.button} onClick={handleChangeUser}>
-          Change User
-        </button>
+        <Button variant="contained" onClick={handleOpen}>
+          Open modal
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <h2>Username: {user.username}</h2>
+            <h2>Email: {user.email}</h2>
+            <h2>Password: {user.password}</h2>
+            <h2>Id: {user._id}</h2>
+            <Button variant="contained" onClick={handleChangeUser}>
+              Change User
+            </Button>
+            <br />
+            <Button variant="contained" onClick={handleClose} style={{ marginTop: '20px' }}>
+              Close
+            </Button>
+          </Box>
+        </Modal>
         <p className={styles.countParagraph}>The value of count is {count}</p>
         <div className={styles.buttonContainer}>
           <button onClick={() => dispatch(increment())} className={styles.button}>
