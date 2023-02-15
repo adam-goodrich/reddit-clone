eb4a90c07904c2dec8313a742f4d182577db1cca
bsherbrook <bsherbrook@gmail.com>
2023-02-14 19:56:22 -0500

    RC-11/add basic breakpoint styles for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,9 @@ const Header = () => {
     return (
       <header className={styles.container}>
         <div className={styles.header__logobox}>
-            <button className={styles.logo}>:)__reddit</button>
+            <button className={styles.logo}>:)<span className={styles.logo_pic}>__reddit</span></button>
         </div>
+        <div className={styles.menu_bar}><span className={styles.menu_popular}>Popular</span><span>v</span></div>
         <input type="text" placeholder="Search Reddit" className={styles.search_bar}/>
         <div className={styles.header__right}>
             <button className={styles.header__button}>Get App</button>
